b5b0337fbb19abc5c41892b6e1ba5c8ec2199728
Olivier FONTANNAUD <albustax@gmail.com>
2016-07-08 16:10:07 +0200

    refacto(class): update nexustools class and cap tasks

--- lib/capistrano/nexus.rb
+++ lib/capistrano/nexus.rb
@@ -3,41 +3,74 @@ require 'capistrano/scm'
 require 'net/http'
 
 class NexusTools
-  def self.get_artifact_url(url, repository, group, artifact_id, extension, version)
-    response = Net::HTTP.get_response(URI("#{url}/nexus/service/local/artifact/maven/redirect?g=#{group}&a=#{artifact_id}&e=#{extension}&v=#{version}&r=#{repository}"))
-    response.fetch('location') rescue nil
+
+  def initialize
+    @endpoint = fetch(:nexus_endpoint)
+    @artifact_group = fetch(:nexus_artifact_group)
+    @artifact_name = fetch(:nexus_artifact_name)
+    @artifact_version = fetch(:nexus_artifact_version).upcase
+    @artifact_extension = fetch(:nexus_artifact_extension)
+    @artifact_repository = fetch(:nexus_artifact_repository)
   end
+
+  def get_artifact_uri
+    uri = URI("#{@endpoint}/service/local/artifact/maven/redirect?g=#{@artifact_group}&a=#{@artifact_name}&e=#{@artifact_extension}&v=#{@artifact_version}&r=#{@artifact_repository}")
+    uri
+  end
+
+  def get_artifact_name
+    @artifact_name
+  end
+
+  def get_revision
+    @artifact_version
+  end
+
+  def get_artifact_archive_name
+    artifact_archive_name = "#{@artifact_name}_#{@artifact_version}.#{@artifact_extension}"
+    artifact_archive_name
+  end
+
 end
 
 class Capistrano::Nexus < Capistrano::SCM
   module DefaultStrategy
     def check
       begin
-        url = NexusTools.get_artifact_url(fetch(:nexus_url), fetch(:nexus_repository), fetch(:nexus_group), fetch(:nexus_artifact_id),
-                                          fetch(:nexus_extension), fetch(:nexus_version))
-        if url
-          set(:artifact_url, url)
-          true
-        else
-          puts "Artifact URL is blank"
-          false
+        nexus = NexusTools.new
+        uri = nexus.get_artifact_uri
+        res = Net::HTTP.get_response(uri)
+        case res
+          when Net::HTTPSuccess then
+            true
+          when Net::HTTPRedirection then
+            location = res['location']
+            # follow 10 redirections max
+            fetch(location, 10)
+            true
+          else
+            false
         end
+        set :artifact_url, uri.to_s
+        set :artifact_version, nexus.get_revision
+        set :artifact_archive_name, nexus.get_artifact_archive_name
       rescue StandardError => e
-        puts "Recieved error: #{e}, backtrace: \n#{e.backtrace}"
+        puts "ERROR: #{e}, backtrace: \n#{e.backtrace}"
         false
       end
     end
 
-    def release
-      context.execute :tar, '-xzf', "#{fetch(:nexus_version)}.#{fetch(:nexus_extension)}", '-C', fetch(:release_path)
+    def download
+      context.execute :wget, '--progress=bar:force', '--no-check-certificate -O', "#{fetch(:artifact_archive_name)}", "'#{fetch(:artifact_url)}'"
     end
 
-    def download
-      context.execute :curl, '-o', "#{fetch(:nexus_version)}.#{fetch(:nexus_extension)}", fetch(:artifact_url)
+    def release
+      context.execute :tar, '-xzf', "#{fetch(:artifact_archive_name)}", '-C', fetch(:release_path)
     end
 
     def fetch_revision
-      fetch(:nexus_version)
+      fetch(:artifact_version)
     end
+
   end
 end
